84b6d954f6fc9413050e3ee12ee8397901d0b1fa
arque1393 <aritra1303@hgmail.com>
2022-08-20 10:06:54 +0530

    adding Round robin

--- OS/SchedulingAlgorithm/ProcessManagement.c
+++ OS/SchedulingAlgorithm/ProcessManagement.c
@@ -1,18 +1,59 @@
+#pragma once 
 #include <stdio.h>
 #include <stdlib.h>
-#define MAX_AT 10
-#define MAX_BT 20
-#define MAX_PR 5
 
+#define MAX_AT 10 // 1-10 
+#define MAX_BT 20 // 1-20
+#define MAX_PR 5  //  1-5
+#define TQ(n) (1<<(n))
 typedef struct
 {
     int ID, AT, PR, BT, RT, TT, WT;
 } Process;
-short write_process_as_binary(Process *p, size_t n, char *path);
+
+// Random Process generator 
 void random_process_generator(Process *p, size_t n);
+//IOn operations 
+short write_process_as_binary(Process *p, size_t n, char *path);
+short read_process_as_binary(Process *p, size_t n, char *path); // not implemented 
 short write_process(Process *p, size_t n, char *path);
 short read_process(Process *p, size_t n, char *path);
 void printProcess(Process p[], int N);
+// find process maximum Burst time
+int find_max_BT(Process p[], int N);
+// sorting 
+void sort_as_burst_time(Process p[], int n);
+void sort_as_priority(Process p[], int n);
+void sort_as_arrival_time(Process p[], int n);
+
+
+
+
+
+
+
+int find_max_BT(Process p[], int n)
+{
+    int i =0,j;
+    for(i=0,j=0;i<n;i++)
+    {
+        if(p[i].BT>p[j].BT)j=i;
+    }
+    return j;
+}
+void sort_as_burst_time(Process p[], int n)
+{
+    int i, j;
+    Process key;
+    for (i = 1; i < n; i++)
+    {
+        key = p[i];
+        j = i - 1;
+        while (key.BT < p[j].BT && j >= 0)
+            p[j-- + 1] = p[j];
+        p[j + 1] = key;
+    }
+}
 
 short write_process_as_binary(Process *p, size_t n, char *path)
 {
@@ -84,13 +125,13 @@ void printProcess(Process p[], int N)
     }
 }
 
-int main()
-{
-    Process P[13];
-    int N = 13;
-    // random_process_generator(P, N);
-    char filename[] = "/run/media/aritrarc/96532fef-218c-4ef7-826c-09b7d7c85016/home/aritrarc/OS/OS/Day5/ProcessInfo.txt";
-    // write_process(P, N, filename); // return 1 If Writing become successfull
-    read_process(P, N, filename);
-    printProcess(P, N);
-}
+// int main()
+// {
+//     Process P[13];
+//     int N = 13;
+//     // random_process_generator(P, N);
+//     char filename[] = "/run/media/aritrarc/96532fef-218c-4ef7-826c-09b7d7c85016/home/aritrarc/OS/OS/Day5/ProcessInfo.txt";
+//     // write_process(P, N, filename); // return 1 If Writing become successfull
+//     read_process(P, N, filename);
+//     printProcess(P, N);
+// }
--- OS/SchedulingAlgorithm/SchedulingAlgo.c
+++ OS/SchedulingAlgorithm/SchedulingAlgo.c
@@ -1,53 +1,10 @@
 #include <stdio.h>
-typedef struct
-{
-    int ID, AT, PR, BT, RT, TT, WT;
-
-} Process;
+#include "ProcessManagement.c"
 Process q[10];
 int N = 5;
 
 double fcfswt, fcfsta, sjfwt, sjfta;
-void InputProcess(Process p[], int n)
-{
-    p[0].BT = 7;
-    p[1].BT = 10;
-    p[2].BT = 20;
-    p[3].BT = 13;
-    p[4].BT = 2;
-    p[5].BT = 17;
-    p[0].ID = 1;
-    p[1].ID = 2;
-    p[2].ID = 3;
-    p[3].ID = 4;
-    p[4].ID = 5;
-    p[5].ID = 6;
-}
 
-void printProcess(Process p[], int N)
-{
-    int i = 0;
-    printf("PID \t AT \t PR \t BT \t RT \t TA \t WT\n");
-    for (i = 0; i < N; i++)
-    {
-        printf("P%d \t %d \t %d \t %d \t %d \t %d \t %d \n",
-               p[i].ID, p[i].AT, p[i].PR, p[i].BT, p[i].RT, p[i].TT, p[i].WT);
-    }
-}
-
-void sort(Process p[], int n)
-{
-    int i, j;
-    Process key;
-    for (i = 1; i < n; i++)
-    {
-        key = p[i];
-        j = i - 1;
-        while (key.BT < p[j].BT && j >= 0)
-            p[j-- + 1] = p[j];
-        p[j + 1] = key;
-    }
-}
 
 void FCFS(Process p[], int n, double *wt, double *ta)
 {
@@ -67,7 +24,36 @@ void FCFS(Process p[], int n, double *wt, double *ta)
     *ta = *ta / n;
     *wt = *wt / n;
 }
+void round_robin(Process p[], int n, int *wt, int *ta)
+{
+    int tq = TQ(2),i,j,g=0;
+    int n_round = p[find_max_BT(p, n)].BT;
+    n_round=(n_round%tq)?n_round/tq+1:n_round/tq;
+    for(i=0;i<n_round;i++)
+    {
+        
+        for(j=0;j<n;j++)
+        {
+            if(p[j].RT<tq)
+            {
+                g+=p[j].RT;
+                p[j].RT=0;
+                p[j].TT=g;
+                p[j].WT=p[j].TT-p[j].BT;
+            }
+            else
+            {
+                p[j].RT -= tq;
+                g+=tq;
+                
+            }
+        }
 
+    }
+     
+
+
+}
 void SJF(Process p[], int n, double *wt, double *ta)
 {
     int i, j;
@@ -85,7 +71,7 @@ void SJF(Process p[], int n, double *wt, double *ta)
 }
 int main()
 {
-    InputProcess(q, N);
+    read_process(q,N, "ProcessInfo.txt");
     FCFS(q, N, &fcfswt, &fcfsta);
     printf("\n%lf %lf \n\n", fcfswt, fcfsta);
     SJF(q, N, &sjfwt, &sjfta);
